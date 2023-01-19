8b89a5fcdc9a0d6b8a97d84cb133669604de0625
sebahag <101423619+sebahag@users.noreply.github.com>
2023-01-19 14:18:02 +0200

    debug to false

--- DroneWatch/settings.py
+++ DroneWatch/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@mkcr@+l4a-e8$$+rlq(u&(bvh1=noyrnpo-u4cq+k(tty^02='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
